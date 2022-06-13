17ba991f09843cda53e24b88ad9313c9967724ce
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-13 11:01:23 +0200

    secondo commit primo esercizio finito

--- src/Jana60/Jsnack1.java
+++ src/Jana60/Jsnack1.java
@@ -4,26 +4,51 @@ import java.util.Scanner;
 
 public class Jsnack1 {
 
-	public static void main(String[] args) {
-		// Il software deve chiedere per 5 volte all’utente di inserire un numero.
-		//Il programma stampa la somma di tutti i numeri inseriti.
-		//Esegui questo programma in due versioni, con il for e con il while.
+//	public static void main(String[] args) {
+//		// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//		//Il programma stampa la somma di tutti i numeri inseriti.
+//		//Esegui questo programma in due versioni, con il for e con il while.
+	
+	
+	//ESERCIZIO CON IL FOR
+	
+//		Scanner scan = new Scanner(System.in);
+//		int SommaNumeri = 0;
+//		
+//		for (int i = 0; i <= 4; i++) {
+//		System.out.println("INSERISCI UN NUMERO");
+//		int NumeroRichiesto = scan.nextInt();
+//		SommaNumeri = (SommaNumeri + NumeroRichiesto);
+//		
+//	
+//		}
+//		
+//		System.out.println( SommaNumeri);
+//		
+//		
+//		
+//		
+//		scan.close();
+//	}
+	
+	
+	//ESERCIZIO CON IL WHILE
+	
+	public static void main(String[] args) {		
+		
 		Scanner scan = new Scanner(System.in);
-		int SommaNumeri = 0;
-		for (int i = 0; i <= 4; i++) {
-		System.out.println("INSERISCI UN NUMERO");
+		
+		int contatore = 0 ;
+		int SommaNumeri = 0 ;
+		
+		while(contatore <= 4) {System.out.println("INSERISCI UN NUMERO");
+		
 		int NumeroRichiesto = scan.nextInt();
 		SommaNumeri = (SommaNumeri + NumeroRichiesto);
-		
-	
-		}
-		
-		System.out.println( SommaNumeri);
-		
-		
-		
-		
-		scan.close();
+		 contatore++;
 	}
-
+		System.out.println(SommaNumeri);
+		scan.close();
+  }
+	
 }
