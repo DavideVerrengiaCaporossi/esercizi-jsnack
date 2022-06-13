2b6ad7d9edee92c4eb03f1e9be0d9a5cf52e729e
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-13 11:54:44 +0200

    piccola modifica

--- src/Jana60/Jsnack3.java
+++ src/Jana60/Jsnack3.java
@@ -7,7 +7,7 @@ public class Jsnack3 {
 	public static void main(String[] args) {
 		// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, genera una falsa lista di 3 invitati.
 		
-		String[] Nomi = {"alfredo","giovanni","dorito","doraemon"};
+		String[] Nomi = {"alfredo","vegeta","Kakarot","doraemon"};
 		String[] Cognomi = {"flash","blob","fium","sbam"};
 		
   Random random = new Random();
---
+++ src/Jana60/Jsnack4.java
@@ -0,0 +1,9 @@
+package Jana60;
+
+public class Jsnack4 {
+
+	public static void main(String[] args) {
+		//Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+	}
+
+}
