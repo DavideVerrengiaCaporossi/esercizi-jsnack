6b8f867910da1c52618ce7ef790445a2ec206dc5
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-13 11:24:48 +0200

    secondo j snack

---
+++ src/Jana60/Jsnack2.java
@@ -0,0 +1,29 @@
+package Jana60;
+
+
+import java.util.Scanner;
+
+public class Jsnack2 {
+
+	public static void main(String[] args) {
+		// Chiedi all'utente di inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int NumUtente = 0;
+		
+	
+		System.out.println("Inserisci un numero");
+	    NumUtente = scan.nextInt();
+		
+		if (NumUtente % 2 == 0) {
+			System.out.println(NumUtente);
+		}
+		else {
+			System.out.println(NumUtente +1 );
+		
+	}
+		
+	scan.close();
+	}
+}
